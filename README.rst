1158b875fca324ddc566c3495676188bc1588b24
David Szotten <davidszotten@gmail.com>
2013-10-19 23:15:53 +0100

    add some scaffolding

--- README.rst
+++ README.rst
@@ -3,3 +3,8 @@ Django postgres age filter
 
 Helper to add age based filtering to django, leveraging postgresql's
 `date functions <http://www.postgresql.org/docs/9.3/static/functions-datetime.html>`_
+
+License
+-------
+
+MIT. See LICENSE for details.
---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-pg-agefilter",
+    version = "0.0.1",
+    author = "David Szotten",
+    author_email = "davidszotten@gmail.com",
+    description = "Helpers to leverage postgres's age filter from django",
+    license = "MIT",
+    url = "https://github.com/davidszotten/django_pg_agefilter",
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    long_description=read('README'),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+    ],
