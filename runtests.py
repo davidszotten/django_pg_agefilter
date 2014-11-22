1af08e32968f8e40d5fa42c110677345c6d7731b
David Szotten <davidszotten@gmail.com>
2014-11-22 11:47:18 +0000

    trying travis

--- runtests.py
+++ runtests.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import django
 from django.core.management import call_command
 
 
@@ -9,4 +10,10 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 if __name__ == '__main__':
     args = sys.argv[1:]
+    try:
+        setup = django.setup
+    except AttributeError:
+        pass  # django < 1.7
+    else:
+        setup()
     call_command('test', *args)
