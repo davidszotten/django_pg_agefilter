38b7fb18b1115130457349b5b44b07bb9a60f5dd
David Szotten <davidszotten@gmail.com>
2017-05-15 22:59:08 +0100

    we can support django 1.11

--- runtests.py
+++ runtests.py
@@ -10,10 +10,5 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    try:
-        setup = django.setup
-    except AttributeError:
-        pass  # django < 1.7
-    else:
-        setup()
+    django.setup()
     call_command('test', *args)
